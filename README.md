f32fd6e05eeefc544ed5a27642f371973bab212e
xiao.ming <1210919685@qq.com>
2021-09-22 15:50:20 +0800

    增加工程模板

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# boost-cmake-learn
-boost学习
+## boost-cmake-learn
+boost学习记录
+
+## 模块说明
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, Boost!" << std::endl;
+    
+    return 0;
+}
